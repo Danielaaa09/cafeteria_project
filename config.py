e363ec5746d1f2d2a5164abbef11d20d40e81767
Danielaaa09 <daniepardo14@gmail.com>
2025-08-12 13:52:56 -0500

    preparado proyecto para despliegue

--- config.py
+++ config.py
@@ -1,4 +1,27 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/cafeteria_db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'supersecretkey'
+from pydantic_settings import BaseSettings # type: ignore
+
+class Settings(BaseSettings):
+    DATABASE_HOST: str
+    DATABASE_PORT: str
+    DATABASE_USER: str
+    DATABASE_PASSWORD: str
+    DATABASE_NAME: str
+    DATABASE_URL: str | None = None  # Explicitamente opcional
+
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE: int = 30
+    APP_PORT: int = 8001
+
+    class Config:
+        env_file = ".env"  
+        env_file_encoding = "utf-8"
+        extra = "ignore"  
+
+    @property
+    def constructed_database_url(self) -> str:
+        if self.DATABASE_URL:
+            return self.DATABASE_URL
+        return f"postgresql+psycopg2://{self.DATABASE_USER}:{self.DATABASE_PASSWORD}@{self.DATABASE_HOST}:{self.DATABASE_PORT}/{self.DATABASE_NAME}"
+
+settings = Settings()
