4d83faf4f5b171b4e5d4a1c1d008cb5de6842c39
Danielaaa09 <daniepardo14@gmail.com>
2025-10-02 13:37:49 -0500

    notificacciones hechas

--- routes/reserva_routes.py
+++
@@ -1,8 +0,0 @@
-# Ejemplo en tu routes/reserva_routes.py
-from flask import Blueprint, render_template
-
-reserva_routes = Blueprint('reserva_routes', __name__)
-
-@reserva_routes.route('/reserva')
-def reserva():
-    return render_template('reserva.html')
